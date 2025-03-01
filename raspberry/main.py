272441f9d346ce2625f70703ed3ed69e58a4621a
israeloriente <israelnunesoriente@gmail.com>
2025-03-01 19:56:18 +0000

    dqw

--- raspberry/main.py
+++ raspberry/main.py
@@ -60,6 +60,7 @@ def processar_comando(command):
 try:
    while True:
         weight = hx.get_weight(5) / 1000
+        print(weight)
 
         # Cria o JSON com os dados do HX711
         data = {
